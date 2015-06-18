e9778d36ff7cf81db18dcaca0ecbceb09513d5e0
bkvanvig <bkvanvig104@yahoo.com>
2015-06-17 19:08:31 -0500

    ran Doxygen, close #22

---
+++ html/search/variables_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['cache',['cache',['../Collatz_8c_09_09.html#a117b8639e485388b199bd815b5490f1e',1,'cache():&#160;Collatz.c++'],['../SphereCollatz_8c_09_09.html#a117b8639e485388b199bd815b5490f1e',1,'cache():&#160;SphereCollatz.c++']]]
+];
